59e3274b0ebf094ddec301bd49120ff72f8db5b6
Resool_2557 <vasanthreddy4557@gmail.com>
2026-03-14 22:53:29 +0530

    Initial Spring Boot MVC Skeleton

---
+++ src/main/java/com/vsms/vehicle_service_system/VehicleServiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vsms.vehicle_service_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleServiceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsms/vehicle_service_system/VehicleServiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsms.vehicle_service_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
